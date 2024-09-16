efed70c7c3baf17c999064bccd4858c3a12276bb
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-09-16 11:26:55 +0200

    Format

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ export { PickIn3D, ResiduesSelect } from "./toggles.js";
 export { CopyToClipBoardIcon } from "./CopyToClipBoardIcon.js";
 export { LinkToFile } from "./LinkToFile.js";
 export { HiddenFileInput } from "./HiddenFileInput.js";
-export { DialogViewer } from "./DialogViewer.js"
-export { ClusterTable } from './table/ClusterTable.js';
-export { StructureTable } from './table/StructureTable.js';
+export { DialogViewer } from "./DialogViewer.js";
+export { ClusterTable } from "./table/ClusterTable.js";
+export { StructureTable } from "./table/StructureTable.js";
 // Other components and types can be imported using
 // for example
 // import { useChunked } from "@i-vresse/haddock3-ui/useChunked"
