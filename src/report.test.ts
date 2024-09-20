6de31d1030f15e816bdf08caaaefc1b2303f2355
sverhoeven <s.verhoeven@esciencecenter.nl>
2024-09-20 10:53:20 +0200

    Add test + sync header and clusters

---
+++ src/report.test.ts
@@ -0,0 +1,73 @@
+
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
+import { renderClusterTable, renderStructureTable } from "./report.js";
+import { page } from "@vitest/browser/context";
+
+let root: HTMLElement;
+
+beforeEach(() => {
+    document.body.innerHTML = '<div id="root"></div>';
+    const nroot = document.getElementById('root');
+    if (!nroot) {
+        throw new Error('Root element not found');
+    }
+    root = nroot;
+})
+
+afterEach(() => {
+    document.body.innerHTML = '';
+})
+
+describe("renderClusterTable()", () => {
+  test('should render a ClusterTable component inside the specified container', () => {
+    renderClusterTable(root, [{
+        key: "id",
+        label: "ID",
+    },
+        {
+            key: "rank",
+            label: "Rank",
+            sorted: "asc",
+          },
+          {
+            key: "size",
+            label: "Size",
+          },
+    ], [
+     {
+        id: 1,
+        rank: 1,
+        size: 2
+     },
+        {
+            id: 2,
+            rank: 2,
+            size: 3
+        }   
+    ]);
+    // TODO wait for the component to render
+    // TODO check if the component is rendered
+    // expect(document.body.innerText.includes('Rank')).toBeTruthy();
+  })
+})
+
+describe('renderStructureTable()', () => {
+    test('should render a StructureTable component inside the specified container', () => {
+        renderStructureTable(root, [{
+            key: "rank",
+            label: "Rank"
+        }, {
+            key: "model",
+            label: "Model"
+        }], [
+            {
+                rank: 1,
+                model: 'model1',
+            },
+            {
+                rank: 2,
+                model: 'model2',
+            }
+        ]);
+    })
+})
